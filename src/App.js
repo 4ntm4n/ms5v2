a58804dbb99aa59d8eb93d7c19c1d6ee3abc9c05
Anton Askling <anton.askling@gmail.com>
2023-05-02 00:43:35 +0200

    import route and routes, add mock routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar/>
-    </div>
+
+      <Routes>
+        {/* unprotected routes */}
+        <Route path="/"/>
+        <Route path="/login"/>
+        <Route path="/signup"/>
+
+
+        {/* protected routes here */}
+      </Routes>
+      
+    </>
   );
 }
 
