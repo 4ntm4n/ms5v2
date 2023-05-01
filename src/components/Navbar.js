01aa62d62b04d35e77c8c44e03a2963bba4f9c10
Anton Askling <anton.askling@gmail.com>
2023-05-02 00:58:46 +0200

    add login state and nav items based on login status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Navbar({children}) {
+function Navbar({ children }) {
+    const [login, setLogin] = useState(false);
+
+    const authNav = (
+        <>
+          <li><a>Groups</a></li>
+          <li><a>Tasks</a></li>
+          <li><a>Log out</a></li>
+        </>
+    );
+    const unAuthNav = (
+        <>
+          <li><a>Log in</a></li>
+          <li><a>signup</a></li>
+        </>
+    );
+
   return (
     <div className="drawer">
   <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
@@ -12,12 +28,11 @@ function Navbar({children}) {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
       </div> 
-      <div className="flex-1 px-2 mx-2">Navbar Title</div>
+      <div className="flex-1 px-2 mx-2">GroupTask</div>
       <div className="flex-none hidden lg:block">
         <ul className="menu menu-horizontal">
           {/* Navbar menu content here */}
-          <li><a>Navbar Item 1</a></li>
-          <li><a>Navbar Item 2</a></li>
+          {login ? (authNav) : (unAuthNav)}
         </ul>
       </div>
     </div>
@@ -28,9 +43,7 @@ function Navbar({children}) {
     <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
     <ul className="menu p-4 w-80 bg-base-100">
      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-      
+     {login ? (authNav) : (unAuthNav)}
     </ul>
     
   </div>
