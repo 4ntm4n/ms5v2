2750db2a4c340b4135a0b73bf9ee8388180598a1
Anton Askling <anton.askling@gmail.com>
2023-05-02 20:22:40 +0200

    install axois and define the axiosInstance functionality

---
+++ src/api/AxiosInterceptors.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+/* 
+ * 
+ * purpose of this interceptors file is to build one axios instance
+ * that has both an request AND a response interceptor that works together in
+ * a single request, on every requiest, appose to having two separate instances
+ * as shown in the walkthrough project at code institute. */
+
+//create baseURL
+
+//create axios instance
+
+//function to refresh token and notify subscribers
+
+//function to subscribe to token refresh
+
+//create request interceptor for updating token
+
+//create response interceptor to retry if a request fails
+
+
+//export axios instance
+
