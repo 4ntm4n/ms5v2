e74cac4ef8c1c882899710e1eb1b9d4e87143f65
Anton Askling <anton.askling@gmail.com>
2023-05-02 14:52:41 +0200

    define useAuth hook and outline methods inside the Auth Provider.

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,10 +1,17 @@
-const { createContext } = require("react");
+const { createContext, useContext } = require("react");
 
 // create an auth context
 const AuthContext = createContext();
 
-//export authcontext as a useAuth-hook.
+//exports authContext as a hook called useAuth.
+export const useAuth = () => useContext(AuthContext);
 
 //create auth provider
-
+export const AuthProvider = () => {
+    //add login function
+    //add logout function
+    //set tokens in local Storage
+    //set user based on decrypted authtoken 
+    // define authcontext as return of authprovider to use as context for protected routes. 
+};
 
