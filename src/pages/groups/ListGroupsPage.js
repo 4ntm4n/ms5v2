c7b283f51451dadf38eb14bb61959a7094f80e8e
Anton Askling <anton.askling@gmail.com>
2023-05-02 22:24:35 +0200

    fix: chage useEffect dependency to [], add setGroups after fetch and map over groups

--- src/pages/groups/ListGroupsPage.js
+++ src/pages/groups/ListGroupsPage.js
@@ -2,27 +2,31 @@ import React, { useEffect, useState } from "react";
 import api from "../../api/AxiosInterceptors";
 
 function ListGroupsPage() {
-  const [groups, setGroups] = useState();
+  const [groups, setGroups] = useState([]);
 
   // fetch groups from the db
   const fetchGroups = async () => {
     try {
         const { data } = await api.get("/groups/");
-        console.log(data);
+        console.log(data.results);
+        setGroups(data.results);
     } catch (error) {
         console.log(error);       
     }
   };
   //  init fetch groups with useEffect on group change
-     
   useEffect(() => {
     fetchGroups();
-  }, [groups]);
+  }, []);
   return (
     <>
       <h1>list of groups you are in</h1>
       {/* map all groups fetch from the db */}
-
+        {groups.length 
+            ? (groups.map(group => (
+                <h1 key ={group.id}>{group.name}</h1>
+            ))) 
+            : ("loading")}
       {/* import add group method somewhere */}
     </>
   );
