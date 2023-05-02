90c976799e2a899727af96eed551eed740a89359
Anton Askling <anton.askling@gmail.com>
2023-05-02 15:48:09 +0200

    add fetch request to token endpoint and log out response status code

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,34 +7,50 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 //create auth provider
-export const AuthProvider = ({children}) => {
-    const [tokens, setTokens] = useState();
-    const [loading, setLoading] = useState(true);
+export const AuthProvider = ({ children }) => {
+  const [tokens, setTokens] = useState();
+  const [loading, setLoading] = useState(true);
 
+  const login = async (e) => {
+    e.preventDefault();
 
-    const login = async (e) => {
-        e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8000/api/token/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: e.target.username.value,
+          password: e.target.password.value,
+        }),
+      });
 
-        try {
-            console.log(`username ${e.target.username.value}, password ${e.target.password.value}`);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      const data = await response.json();
+      console.log("response status :", response.status);
 
-    useEffect(() => {
-        setLoading(false);
-    }, []);
-    //add logout function
-    //set tokens in local Storage
-    //set user based on decrypted authtoken 
+      console.log(
+        `username ${e.target.username.value}, password ${e.target.password.value}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const authData = {
-        login,
-    };
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+  //add logout function
+  //set tokens in local Storage
+  //set user based on decrypted authtoken
 
-    return (
-        <AuthContext.Provider value={authData}>{loading ? null : children}</AuthContext.Provider>
-    ); 
+  const authData = {
+    login,
+  };
+
+  return (
+    <AuthContext.Provider value={authData}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
 };
-
