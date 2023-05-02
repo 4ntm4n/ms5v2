6f1b156a78a0684beb543bd6de20d103c5242621
Anton Askling <anton.askling@gmail.com>
2023-05-02 20:39:42 +0200

    outline all functionality I want in the axiosInstance

--- src/api/AxiosInterceptors.js
+++ src/api/AxiosInterceptors.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-/* 
- * 
+/*
+ *
  * purpose of this interceptors file is to build one axios instance
  * that has both an request AND a response interceptor that works together in
  * a single request, on every requiest, appose to having two separate instances
@@ -9,26 +9,42 @@ import axios from "axios";
 
 //get tokens from localStorage
 let tokens = localStorage.getItem("tokens")
-    ? JSON.parse(localStorage.getItem("tokens"))
-    : null;
+  ? JSON.parse(localStorage.getItem("tokens"))
+  : null;
 
 //create baseURL
 const baseURL = "http://localhost:8000";
 
 //create axios instance
 const api = axiosInstance.create({
-    baseURL,
-    headers: {Authorization: `Bearer $tokens?.access`}
+  baseURL,
+  headers: { Authorization: `Bearer $tokens?.access` },
 });
 
+//define list of subscribers,check if refresh is already in progress
+let isRefreshing = false;
+let refreshSubscribers = [];
+
 //function to refresh token and notify subscribers
+const refreshTokenAndNotifySubscriber = () => {
+    //define method to refresh token here
+};
 
 //function to subscribe to token refresh
-
+const onAccessTokenRefresh = (callback) => {
+    // code to subscribe to a token refresh
+};
 //create request interceptor for updating token
-
+api.interceptor.request.use((config) => {
+    console.log("hello from before a request");
+});
 //create response interceptor to retry if a request fails
-
-
+api.interceptor.response.use(
+    response => response,
+    error => {
+        // handle failed request because of token expired
+        // (aka status 401.) and retry request after token-change
+    }
+);
 //export axios instance
-
+export default api;
