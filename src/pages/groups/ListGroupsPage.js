c13849eac954283ed2579f6abc20e4a10ce5917c
Anton Askling <anton.askling@gmail.com>
2023-05-02 20:47:43 +0200

    add ListGroupsPage and outline the funcs I want

---
+++ src/pages/groups/ListGroupsPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ListGroupsPage() {
+
+  // fetch groups from the db
+
+  //  init fetch groups with useEffect on group change
+
+
+  return (
+    <>
+      <h1>list of groups you are in</h1>
+      {/* map all groups fetch from the db */}
+
+      {/* import add group method somewhere */}
+    </>
+  );
+}
+
+export default ListGroupsPage;
