928778080330ed1ac7cfb0fa454cbfeec4a06ad3
Anton Askling <anton.askling@gmail.com>
2023-05-03 20:21:53 +0200

    add public routes component, and wrap routes that loged in users should not be able to visit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import Signup from "./pages/auth/Signup";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 import ListGroupsPage from "./pages/groups/ListGroupsPage";
+import PublicRoutes from "./utils/PublicRoutes";
+
 
 function App() {
   return (
@@ -13,11 +16,16 @@ function App() {
         <Routes>
           {/* unprotected routes */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/signup" element={<Signup />} />
+          
+          <Route element={<PublicRoutes />}>
+            <Route path="/login" element={<LoginPage />}/>
+            <Route path="/signup" element={<Signup />} />
+          </Route>
 
           {/* protected routes here */}
-          <Route path="/groups" element={<ListGroupsPage /> } />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/groups" element={<ListGroupsPage /> } />
+          </Route>
         </Routes>
       </Navbar>
     </>
---
+++ src/utils/PublicRoutes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+function PublicRoutes() {
+    const {user} = useAuth();
+    
+    return !user ? <Outlet /> : <Navigate to={-1} replace />; 
+}
+
+export default PublicRoutes;
