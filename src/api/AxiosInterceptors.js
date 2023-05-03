55e22ac15a99de1250b17306fd255870fc5e106f
Anton Askling <anton.askling@gmail.com>
2023-05-03 20:06:40 +0200

    add utils folder and a protectedRoutes component for auth-routes.

--- src/api/AxiosInterceptors.js
+++ src/api/AxiosInterceptors.js
@@ -5,30 +5,28 @@ import axios from "axios";
  * purpose of this interceptors file is to build one axios instance
  * that has both an request AND a response interceptor that works together in
  * a single request, on every requiest, appose to having two separate instances
- * as shown in the walkthrough project at code institute. 
- * If multiple requests are made at the same time, this apprach solves the issue 
+ * as shown in the walkthrough project at code institute.
+ * If multiple requests are made at the same time, this apprach solves the issue
  * of the server being bombarded with requests to the refresh token endpoint,
  * by only letting one request reach the function that makes the request to the refresh
- * endpoint on the server, the rest of the requests will be stored in a promise 
+ * endpoint on the server, the rest of the requests will be stored in a promise
  * and updated once a new refresh token is obtained.*/
 
-//get tokens from localStorage
+
+const baseURL = "http://localhost:8000";
+
 let tokens = localStorage.getItem("tokens")
   ? JSON.parse(localStorage.getItem("tokens"))
   : null;
 
-//create baseURL
-const baseURL = "http://localhost:8000";
-
-//create axios instance
 const api = axios.create({
   baseURL,
   headers: { Authorization: `Bearer ${tokens?.access}` },
 });
 
-//define list of subscribers,check if refresh is already in progress
 let isRefreshing = false;
 let refreshSubscribers = [];
+
 
 /*
  *
@@ -39,25 +37,22 @@ let refreshSubscribers = [];
  * it goes through the list of subscribers to update all of their header info
  * using setting the callback variable. refreshSubscribers are emptied,
  * and finally isRefreshing is toggled back to initial state.*/
-const refreshTokenAndNotifySubscriber = () => {
-  if (!isRefreshing) {
-    isRefreshing = true;
-
-    return axios
-      .post(`${baseURL}/api/token/refresh/`, { refresh: tokens.refresh })
-      .then((response) => {
-        tokens = response.data;
-        localStorage.setItem("tokens", response.data);
-        refreshSubscribers.forEach((callback) =>
-          callback(response.data.access)
-        );
-        refreshSubscribers = [];
-      })
-      .finally(() => {
-        isRefreshing = false;
-      });
+function refreshTokenAndNotifySubscribers() {
+    if (!isRefreshing) {
+      isRefreshing = true;
+      return axios
+        .post(`${baseURL}/api/token/refresh/`, { refresh: tokens.refresh })
+        .then((response) => {
+          tokens = response.data;
+          localStorage.setItem("tokens", JSON.stringify(response.data));
+          refreshSubscribers.forEach((callback) => callback(response.data.access));
+          refreshSubscribers = [];
+        })
+        .finally(() => {
+          isRefreshing = false;
+        });
+    }
   }
-};
 
 //function to subscribe to token refresh
 const onAccessTokenRefresh = (callback) => {
@@ -66,37 +61,46 @@ const onAccessTokenRefresh = (callback) => {
 
 /*
  *
- * the promise in this interceptor waits for a new access token, creates a 
- * callback function that updates the original request with the new access 
- * token and retries the original request; it will not execute until the 
- * refresh token process has completed */
+ * the interceptor before each request checksif tokens exists in memory.
+ * If it doesnt, it tries to get it from the localStorage.
+ * If it doesnt exisit there, the token i set to null */
 api.interceptors.request.use((config) => {
-  console.log("hello from before a request");
-  return config;
-});
-//create response interceptor to retry if a request fails
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    const originalRequest = error.config;
-    const status = error.response.status;
-
-    if (status === 401) {
-      if (!isRefreshing) {
-        refreshTokenAndNotifySubscriber();
-      }
-
-      const retryOriginalRequest = new Promise((resolve) => {
-        onAccessTokenRefresh((newAccessToken) => {
-          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-          resolve(axios(originalRequest));
-        });
-      });
+    if (!tokens) {
+      tokens = localStorage.getItem("tokens")
+        ? JSON.parse(localStorage.getItem("tokens"))
+        : null;
     }
-    return Promise.reject(error);
-  }
-);
+    config.headers.Authorization = `Bearer ${tokens.access}`;
+    return config;
+  });
+
+/*
+ *
+ * the promise in this interceptor waits for a new access token, creates a
+ * callback function that updates the original request with the new access
+ * token and retries the original request; it will not execute until the
+ * refresh token process has completed */
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const { config, response: { status } } = error;
+      const originalRequest = config;
+  
+      if (status === 401) {
+        if (!isRefreshing) {
+          refreshTokenAndNotifySubscribers();
+        }
+        const retryOriginalRequest = new Promise((resolve) => {
+          onAccessTokenRefresh((newAccessToken) => {
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            resolve(axios(originalRequest));
+          });
+        });
+        return retryOriginalRequest;
+      }
+  
+      return Promise.reject(error);
+    }
+  );
 //export axios instance
 export default api;
