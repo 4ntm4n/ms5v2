8ab9394b0763eaf3f8b03d9e20cff3eec5e9bb5d
Anton Askling <anton.askling@gmail.com>
2023-05-04 01:35:26 +0200

    pass group as a prop to groupCard and destructire values.

--- src/components/GroupCard.js
+++ src/components/GroupCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function GroupCard() {
+function GroupCard({ group }) {
+    const { id, name, description, group_owner, members } = group;
   return(
     <div>
         <h1>this card will display group information</h1>
