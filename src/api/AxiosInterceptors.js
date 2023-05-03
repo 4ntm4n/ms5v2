24da4252458ccfc3e68280f916265a9342737066
Anton Askling <anton.askling@gmail.com>
2023-05-03 03:47:12 +0200

    add new retry request prommise and descriptive docstring

--- src/api/AxiosInterceptors.js
+++ src/api/AxiosInterceptors.js
@@ -17,10 +17,9 @@ const baseURL = "http://localhost:8000";
 
 //create axios instance
 const api = axios.create({
-    baseURL,
-    headers: { Authorization: `Bearer ${tokens?.access}` },
-  });
-
+  baseURL,
+  headers: { Authorization: `Bearer ${tokens?.access}` },
+});
 
 //define list of subscribers,check if refresh is already in progress
 let isRefreshing = false;
@@ -28,34 +27,42 @@ let refreshSubscribers = [];
 
 //function to refresh token and notify subscribers
 const refreshTokenAndNotifySubscriber = () => {
-    //define method to refresh token here
+  //define method to refresh token here
 };
 
 //function to subscribe to token refresh
 const onAccessTokenRefresh = (callback) => {
-    // code to subscribe to a token refresh
+  // code to subscribe to a token refresh
 };
 //create request interceptor for updating token
 api.interceptors.request.use((config) => {
-    console.log("hello from before a request");
-    return config;
+  console.log("hello from before a request");
+  return config;
 });
 //create response interceptor to retry if a request fails
 api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    error => {
-        const originalRequest = error.config;
-        const status = error.response.status;
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const originalRequest = error.config;
+    const status = error.response.status;
 
-        if (status === 401) {
-            if (!isRefreshing){
-                console.log("we need to call the refresh token endpoint!");
-            }
-        }
-    return Promise.reject(error);
+    if (status === 401) {
+      if (!isRefreshing) {
+        console.log("we need to call the refresh token endpoint!");
+      }
+
+      /*
+       *
+       * this promise waits for a new access token, creates a callback function 
+       * that updates the original request with the new access token, and retries 
+       * the original request; it will not execute until the refresh token process 
+       * has completed */
+      const retryOriginalRequest = new Promise();
     }
+    return Promise.reject(error);
+  }
 );
 //export axios instance
 export default api;
