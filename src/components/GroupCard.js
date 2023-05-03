6cabb87a469fc131590ba2042daebcd3d1344b23
Anton Askling <anton.askling@gmail.com>
2023-05-04 01:40:55 +0200

    add basic card layout for group

--- src/components/GroupCard.js
+++ src/components/GroupCard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function GroupCard({ group }) {
-    const { id, name, description, group_owner, members } = group;
-  return(
-    <div>
-        <h1>this card will display group information</h1>
+  const { id, name, description, group_owner, members } = group;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Card title!</h2>
+        <p>card content</p>
+        <div className="card-actions justify-end">
+        <Link to={`${id}`}>
+            <button className="btn btn-primary">go to group: {id}</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
