4e36312a4b4a10bd8897e7b87efe0be88a3dfe2f
Anton Askling <anton.askling@gmail.com>
2023-05-05 01:51:15 +0200

    add logic to toggle between add members and members

--- src/pages/groups/GroupDetailPage.js
+++ src/pages/groups/GroupDetailPage.js
@@ -3,22 +3,32 @@ import { useParams } from "react-router-dom";
 import BottomNav from "../../components/BottomNav";
 import api from "../../api/AxiosInterceptors";
 import GroupMembers from "../../components/GroupMembers";
+import AddGroupMembers from "../../components/AddGroupMembers";
 
 function GroupDetailPage() {
   const { id } = useParams();
   const drawerRef = useRef();
   const [group, setGroup] = useState();
+  const [addMember, setAddMember] = useState(false);
 
   const handleDrawerToggle = () => {
     drawerRef.current.checked = !drawerRef.current.checked;
   };
 
+  const showAddMember = () => {
+    setAddMember(true);
+  };
+
+  const showMembers = () => {
+    setAddMember(false);
+  };
+
   const fetchGroup = async () => {
     try {
-        const { data } = await api.get(`/groups/${id}/`);
-        setGroup(data);
+      const { data } = await api.get(`/groups/${id}/`);
+      setGroup(data);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   };
 
@@ -28,29 +38,43 @@ function GroupDetailPage() {
 
   return (
     <div className="drawer">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" ref={drawerRef} />
+      <input
+        id="my-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+        ref={drawerRef}
+      />
       <div className="drawer-content">
         {/* Page content here */}
-        <button className="btn" onClick={handleDrawerToggle}>open drawer</button>
-        <BottomNav openDrawer={handleDrawerToggle}/>
+        <button className="btn" onClick={handleDrawerToggle}>
+          open drawer
+        </button>
+        <BottomNav openDrawer={handleDrawerToggle} />
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
         <div className="menu p-4 w-80 bg-base-100 text-base-content">
           {/* Sidebar content here */}
           <div className="btn-group flex justify-center">
-          <button className="btn px-8 btn-active" >
-                members
-              </button>
-              <button className="btn " >
-                add members
-              </button>
+            <button 
+                onClick={showMembers} 
+                className="btn px-8 btn-active">
+                members</button>
+            <button 
+                onClick={showAddMember} 
+                className="btn">
+              add members
+            </button>
           </div>
+          <div className="divider"></div>
+          <div>
+            {addMember ? (
+              <AddGroupMembers />
+            ) : (
+                group && <GroupMembers members={ group.members} />
+            )}
             <div className="divider"></div>
-            <div>
-                {group && <GroupMembers members={group.members} />}
-                  <div className="divider"></div>
-            </div>
+          </div>
         </div>
       </div>
     </div>
