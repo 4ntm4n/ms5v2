164e685c2243d5e715b1caad2061d43ca92c0656
Anton Askling <anton.askling@gmail.com>
2023-05-04 19:08:08 +0200

    add buttons to toggle between viewing and adding members

--- src/pages/groups/GroupDetailPage.js
+++ src/pages/groups/GroupDetailPage.js
@@ -20,15 +20,18 @@ function GroupDetailPage() {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+        <div className="menu p-4 w-80 bg-base-100 text-base-content">
           {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+          <div className="btn-group flex justify-center">
+          <button className="btn px-8 btn-active" >
+                members
+              </button>
+              <button className="btn " >
+                add members
+              </button>
+          </div>
+
+        </div>
       </div>
     </div>
   );
