1638f8263ed05358a6c08d673246e0fdd734ac94
Anton Askling <anton.askling@gmail.com>
2023-05-04 18:53:37 +0200

    add useRef and handleOpenDrawer function

--- src/pages/groups/GroupDetailPage.js
+++ src/pages/groups/GroupDetailPage.js
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useParams } from "react-router-dom";
 import BottomNav from "../../components/BottomNav";
 
 function GroupDetailPage() {
   const { id } = useParams();
+  const drawerRef = useRef();
 
-  const handleOpenDrawer = () => {
-    
+  const handleDrawerToggle = () => {
+    drawerRef.current.checked = !drawerRef.current.checked;
   };
 
   return (
     <div className="drawer">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" ref={drawerRef} />
       <div className="drawer-content">
         {/* Page content here */}
-        <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-          Open drawer
-        </label>
-
+        <button className="btn" onClick={handleDrawerToggle}>open drawer</button>
         <BottomNav />
       </div>
       <div className="drawer-side">
