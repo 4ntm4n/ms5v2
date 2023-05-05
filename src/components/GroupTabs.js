8266726ccfa28c83d0c6e6364da9aff8c3a551ea
Anton Askling <anton.askling@gmail.com>
2023-05-05 22:48:20 +0200

    add logic to activate tab, update style class to tab-bordered

--- src/components/GroupTabs.js
+++ src/components/GroupTabs.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 
-function GroupTabs() {
+function GroupTabs({ id }) {
+  const [activeTab, setActiveTab] = useState("");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <nav className="flex justify-center bg-primary-500 py-2 mt-2">
-      <NavLink to=""  className="tab tab-lg tab-lifted mx-2">Tab 1</NavLink>
-      <NavLink to="active" className="tab tab-lg tab-lifted mx-2 tab-active">Tab 2</NavLink>
-      <NavLink to="completed" className="tab tab-lg tab-lifted mx-2">Tab 3</NavLink>
+      <NavLink
+        to=""
+        className={`tab tab-lg tab-bordered mx-2 ${
+          activeTab === "" ? "tab-active" : ""
+        }`}
+        onClick={() => handleTabClick("")}
+      >
+        Tab 1
+      </NavLink>
+      <NavLink
+        to="active"
+        className={`tab tab-lg tab-bordered mx-2 ${
+          activeTab === "active" ? "tab-active" : ""
+        }`}
+        onClick={() => handleTabClick("active")}
+      >
+        Tab 2
+      </NavLink>
+      <NavLink
+        to="completed"
+        className={`tab tab-lg tab-bordered mx-2 ${
+          activeTab === "completed" ? "tab-active" : ""
+        }`}
+        onClick={() => handleTabClick("completed")}
+      >
+        Tab 3
+      </NavLink>
     </nav>
   );
 }
