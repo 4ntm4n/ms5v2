10408010cc6be97634795da41b137f5a583df347
Anton Askling <anton.askling@gmail.com>
2023-05-06 23:07:08 +0200

    copy over unAssigned code to completed and inProgress components to use as boilerplate.

--- src/pages/groups/grouptasks/Completed.js
+++ src/pages/groups/grouptasks/Completed.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Task from "../../../components/Task";
 
 function Completed() {
-  return <div>Completed</div>;
+  const { id } = useParams();
+  const [completedTasks, setcompletedTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await api.get(`/tasks/?owning_group__id=${id}`);
+      console.log(data.results);
+      setcompletedTasks(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <>
+      {completedTasks.length
+        ? completedTasks.map((task) => <Task key={task.id} taskInfo={task} />)
+        : "there are no unitiated tasks"}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 "></div>
+      <div className="container mx-auto"></div>
+    </>
+  );
 }
 
 export default Completed;
--- src/pages/groups/grouptasks/InProgress.js
+++ src/pages/groups/grouptasks/InProgress.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Task from "../../../components/Task";
 
 function InProgress() {
-  return <div>InProgress</div>;
-}
+  const { id } = useParams();
+  const [activeTasks, setactiveTasks] = useState([]);
 
+  const fetchTasks = async () => {
+    try {
+      const { data } = await api.get(`/tasks/?owning_group__id=${id}`);
+      console.log(data.results);
+      setactiveTasks(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <>
+      {activeTasks.length
+        ? activeTasks.map((task) => <Task key={task.id} taskInfo={task} />)
+        : "there are no unitiated tasks"}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 "></div>
+      <div className="container mx-auto"></div>
+    </>
+  );
+}
 export default InProgress;
