561cf04685854ec9fd0540c22c864da486d4f443
Anton Askling <anton.askling@gmail.com>
2023-05-08 02:43:39 +0200

    redesign task card

--- src/components/Task.js
+++ src/components/Task.js
@@ -22,78 +22,33 @@ function Task({ taskInfo }) {
 
   return (
     <>
- <div className="w-[90%] mx-auto shadow-xl rounded-lg">
-  <div className="bg-yellow-400 p-4 flex justify-between" onClick={toggleCollapse}>
-    <h2>{title}</h2>
-  </div>
-  <div
-    className={`overflow-hidden transition-all duration-500 ease-in-out overflow-y-auto ${
-      isCollapsed ? "max-h-0" : "max-h-[350px]"
-    }`}
-  >
-    <div className="p-4 opacity-100">
-      <p>
-        {description}
-      </p>
-    </div>
-  </div>
-</div>
-      {/*  <div className="collapse w-[90%] mx-auto rounded-sm relative">
-  <input type="checkbox" className="peer" />
-  <div className="flex collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-    <div className="bg-yellow-500 flex w-8">
-      <div className="avatar placeholder min-w-[16px] bg-yellow-200 self-center">
-        <div className="bg-neutral-focus text-neutral-content rounded-full w-8">
-          <span className="text-xs">AA</span>
+      <div className="w-[90%] mx-auto shadow-xl rounded-lg">
+        <div className="flex justify-around rounded-lg">
+          <div className="w-1/6 flex items-center">
+            <div className="avatar">
+              <div className="w-12 rounded-full">
+                <img src="https://picsum.photos/200/200" />
+              </div>
+            </div>
+          </div>
+          <div className=" p-4 w-4/6" onClick={toggleCollapse}>
+            <h2 className="text-lg text-center">{title}</h2>
+          </div>
+          <div className="btn w-12 mt-2">
+            <FontAwesomeIcon icon={faEllipsisVertical} />
+          </div>
+        </div>
+
+        <div
+          className={`overflow-hidden transition-all duration-300 ease-in-out overflow-y-auto ${
+            isCollapsed ? "max-h-0" : "max-h-[350px]"
+          }`}
+        >
+          <div className="p-4 opacity-100">
+            <p>{description.length ? description : "no description added."}</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="self-center mx-2 mr-8">{title}</div>
-  </div>
-  <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-    <p>{description}</p>
-  </div>
-  <div className="absolute top-0 right-0 z-50">
-    <div className="dropdown dropdown-left btn btn-primary font-semibold px-2 py-1 rounded">
-      <label tabIndex={0} className="btn m-1">
-        <FontAwesomeIcon icon={faEllipsisVertical} />
-      </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a>Delete Task</a>
-        </li>
-        <li>
-          <label htmlFor="edit-group-modal">Edit group info</label>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div> */}
-
-      {/*  <div
-        tabIndex={0}
-        className="collapse rounded-xl w-[90%] bg-red-500 mx-auto p-0"
-      >
-        <div className="collapse-title rounded-t-xl p-3 flex justify-between items-center">
-          <div className="font-medium truncate w-[75%]">{title}</div>
-          <div className="flex flex-wrap justify-around w-auto bg-yellow-400">
-            <button className="md:block hidden btn font-semibold text-xs px-2 py-1 rounded">
-              Take Ownership
-            </button>
-            <button className="btn btn-primary font-semibold px-2 py-1 rounded">
-              <></>
-            </button>
-          </div>
-        </div>
-        <div className="collapse-content">
-          <div className="">
-            <p className="">{description}</p>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
