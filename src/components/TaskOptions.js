53779fa143f8cb186daa85378b02b456f3bf1499
Anton Askling <anton.askling@gmail.com>
2023-05-09 02:20:17 +0200

    add handleRelease function that lets user re-open closed or completed tasks.

--- src/components/TaskOptions.js
+++ src/components/TaskOptions.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import api from "../api/AxiosInterceptors";
 import UpdateTaskModal from "./UpdateTaskModal";
 function TaskOptions({ taskInfo, updateTasks }) {
-  const { id, title, description, in_progress, completed, owner } = taskInfo;
+  const { id, in_progress, completed, owner } = taskInfo;
 
   const handleDelete = async () => {
     try {
@@ -48,6 +48,20 @@ function TaskOptions({ taskInfo, updateTasks }) {
     }
   };
 
+  const handleRelease = async () => {
+    try {
+      await api.patch(`tasks/${id}/`, {
+        completed: false,
+        in_progress: false,
+        owner: null,
+      });
+      updateTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   return (
     <>
       <div className="dropdown dropdown-left ">
@@ -59,31 +73,43 @@ function TaskOptions({ taskInfo, updateTasks }) {
           className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
         >
           <li>
-            {in_progress ? (
+            {completed ? null : in_progress ? (
               <a
-                className="bg-primary
-                text-white hover:bg-base-300 
-                hover:text-black"
+                className="bg-primary text-white hover:bg-base-300 hover:text-black"
                 onClick={handleComplete}
               >
                 Mark as complete
               </a>
             ) : (
               <a
-                className="bg-primary
-                text-white hover:bg-base-300 
-                hover:text-black"
+                className="bg-primary text-white hover:bg-base-300 hover:text-black"
                 onClick={handleClaim}
               >
                 Take ownership
               </a>
             )}
           </li>
+          
+          {completed ? (
+            <li>
+              <a className="text-error" onClick={handleRelease}>
+                re-open
+              </a>
+            </li>
+          ) : (
+            <li>
+              <label htmlFor={`edit-task-modal${id}`}>Edit task info</label>
+            </li>
+          )}
 
-          <li>
-          </li>
 
-        
+          {in_progress && !completed && (
+            <li>
+              <a className="text-error" onClick={handleRelease}>
+                release to group
+              </a>
+            </li>
+          )}
 
           <li>
             <a className="text-error" onClick={handleDelete}>
