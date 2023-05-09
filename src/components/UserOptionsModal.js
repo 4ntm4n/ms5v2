e9962b3a84cea2e7811e7d757e8fb6398744f40e
Anton Askling <anton.askling@gmail.com>
2023-05-09 22:39:25 +0200

    add profile image picker method, style modal

--- src/components/UserOptionsModal.js
+++ src/components/UserOptionsModal.js
@@ -1,32 +1,76 @@
 import React, { useRef, useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 
-function UserOptionsModal({ user }) {
-  const { userId, username, image } = user;
+function UserOptionsModal() {
+  const {user} = useAuth();
   const imgInputRef = useRef(null);
   const [profileImg, setProfileImg] = useState({
     img: null,
   });
+
+ 
   const modalCheckRef = useRef(null);
-  
+
   const modalToggle = () => {
     modalCheckRef.current.checked = !modalCheckRef.current.checked;
   };
 
+  const triggerImgChoice = () => {
+    if (imgInputRef.current) {
+      imgInputRef.current.click();
+    }
+  };
+
+  const handleImagePick = (event) => {
+    setProfileImg({
+      img: event.target.files[0],
+    });
+  };
+
   return (
     <>
-      <input ref={modalCheckRef} type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <input
+        ref={modalCheckRef}
+        type="checkbox"
+        id="my-modal-6"
+        className="modal-toggle"
+      />
+        
       <div className="modal modal-bottom sm:modal-middle">
-        <form className="modal-box">
-          <input 
+        <form className="modal-box ">
+            <div className="flex flex-col place-items-center gap-4" >
+          <input
             type="file"
-            className="hidden"
+             className="hidden" 
             accept="image/*"
             ref={imgInputRef}
+            onChange={handleImagePick}
           />
-            <button className="btn btn-sm btn-primary" onClick={modalToggle} type="submit">update profile img</button>
-
-          <div className="modal-action">
-            <button onClick={modalToggle} >close</button>
+            <h3>choose a new profile picture</h3>
+          <div
+            className="w-[120px] 
+                       h-[120px] 
+                     text-white 
+                     bg-slate-400
+                     flex 
+                     place-items-center
+                     justify-center
+                     "
+          >
+            choose image
+          </div>
+         
+          
+          </div>
+          <div className="modal-action justify-self-end">
+            <button className="btn btn-outline" onClick={modalToggle}>close</button>
+            <button
+            className="btn btn-md btn-primary"
+            onClick={modalToggle}
+            type="submit"
+          >
+            update profile img
+          </button>
           </div>
         </form>
       </div>
