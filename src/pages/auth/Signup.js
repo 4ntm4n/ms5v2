fa6a0dc144021c09711a778bb78e1f559dd15fe0
Anton Askling <anton.askling@gmail.com>
2023-05-11 06:03:31 +0200

    add some text and refactor sign up card to be a form

--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -2,23 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Signup() {
+
+  
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign up now!</h1>
+          <h1 className="text-5xl font-bold">You are just one step away</h1>
           <p className="py-6">
-            text here?
+            Sign up now and create tasks together with your collegues
           </p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+          <form className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Username</span>
               </label>
               <input
                 type="text"
+                name="username"
                 placeholder="choose a username"
                 className="input input-bordered"
               />
@@ -29,6 +32,7 @@ function Signup() {
               </label>
               <input
                 type="text"
+                name="password1"
                 placeholder="choose a password"
                 className="input input-bordered"
               />
@@ -39,6 +43,7 @@ function Signup() {
               </label>
               <input
                 type="text"
+                name="password2"
                 placeholder="Repeat Password"
                 className="input input-bordered"
               />
@@ -49,9 +54,9 @@ function Signup() {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
+              <button type="submit" className="btn btn-primary">Sign Up</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
