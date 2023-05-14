e8ce4b78a30eb8c4fc4bfe8181c164ff31ddf84b
Anton Askling <anton.askling@gmail.com>
2023-05-14 18:33:10 +0200

    add images folder and images for background in homepage, add start of backgroundImage Component

---
+++ src/components/BackgroundImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+images = ["/img/homepage/image1.webp"];
+
+function BackgroundImage() {
+  return (
+    <>
+        {images[0]}
+    </>
+  );
+}
+
+export default BackgroundImage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { faListCheck, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 function HomePage() {
-  return <h1>this is the homepage</h1>;
+  return (
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <div className="text-5xl font-bold flex justify-around gap-5 mb-11">
+      <FontAwesomeIcon icon={faUsers} /> 
+        <FontAwesomeIcon icon={faListCheck} /> 
+      </div>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+  );
 }
 
 export default HomePage;
