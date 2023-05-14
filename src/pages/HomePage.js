998f2863c1a377402b9383d251ecc4b1443c2966
Anton Askling <anton.askling@gmail.com>
2023-05-14 20:43:09 +0200

    add button links to signup and login page from homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,22 +2,26 @@ import React from "react";
 import { faListCheck, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BackgroundImage from "../components/BackgroundImage";
-
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-  <BackgroundImage>
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-center text-neutral-content">
-      <div className="max-w-md">
-        <div className="text-5xl font-bold flex justify-around gap-5 mb-11">
-        <FontAwesomeIcon icon={faUsers} /> 
-          <FontAwesomeIcon icon={faListCheck} /> 
+    <BackgroundImage>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+       <div className="flex flex-col ">
+       <div className="text-5xl font-bold flex justify-center gap-5 mb-11">
+          <FontAwesomeIcon icon={faUsers} />
+          <FontAwesomeIcon icon={faListCheck} />
         </div>
-        <button className="btn btn-primary">Get Started</button>
+
+        <div className="flex gap-5">
+          <Link to="/login" className="btn btn-secondary">Log in</Link>
+          <Link to="/signup" className="btn btn-primary">Sign up</Link>
+        </div>
+       </div>
       </div>
-    </div>
-  </BackgroundImage>
+    </BackgroundImage>
   );
 }
 
