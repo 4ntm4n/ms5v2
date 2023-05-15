4863a3f4577252505f0ab537f954d06669814c94
Anton Askling <anton.askling@gmail.com>
2023-05-15 18:26:49 +0200

    fix: change name of unasTasks state to tasks to be more generic

--- src/pages/groups/grouptasks/UnAssigned.js
+++ src/pages/groups/grouptasks/UnAssigned.js
@@ -6,7 +6,7 @@ import AddTaskModal from "../../../components/AddTaskModal";
 
 function UnAssigned() {
   const { id } = useParams();
-  const [unasTasks, setUnasTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const [nextTasks, setNextTasks] = useState([]);
   const [nextUrl, setNextUrl] = useState(null);
   const [tasksChanged, setTasksChanged] = useState(false);
@@ -15,7 +15,9 @@ function UnAssigned() {
     setTasksChanged((prevTasksChanged) => !prevTasksChanged);
   };
 
-  const fetchTasks = async (url = `/tasks/?owning_group__id=${id}&in_progress=false&completed=false`) => {
+  const fetchTasks = async (
+    url = `/tasks/?owning_group__id=${id}&in_progress=false&completed=false`
+  ) => {
     try {
       const { data } = await api.get(url);
       setNextTasks(data.results);
@@ -27,7 +29,7 @@ function UnAssigned() {
 
   const loadMoreTasks = () => {
     if (nextUrl) {
-      setUnasTasks([...unasTasks, ...nextTasks]);
+      setTasks([...tasks, ...nextTasks]);
       fetchTasks(nextUrl);
     }
   };
@@ -40,20 +42,30 @@ function UnAssigned() {
     <div className="pb-[180px]">
       <div className="grid grid-cols-1 ">
         <div className="container mx-auto max-w-xl ">
-          {unasTasks.length
-            ? unasTasks.map((task) => <Task key={task.id} taskInfo={task}  updateTasks={updateTasks} />)
-            : <p className="text-center">there are currently no uninitiated tasks for this group.</p>
-          }
-          {nextTasks.map((task) => <Task key={task.id} taskInfo={task}  updateTasks={updateTasks} />)}
+          {tasks.length ? (
+            tasks.map((task) => (
+              <Task key={task.id} taskInfo={task} updateTasks={updateTasks} />
+            ))
+          ) : (
+            <p className="text-center">
+              there are currently no uninitiated tasks for this group.
+            </p>
+          )}
+          {nextTasks.map((task) => (
+            <Task key={task.id} taskInfo={task} updateTasks={updateTasks} />
+          ))}
         </div>
         {nextUrl && (
-        <button onClick={loadMoreTasks} className="btn btn-secondary max-w-xs mx-auto mt-5">
-          Load more tasks
-        </button>
-      )}
+          <button
+            onClick={loadMoreTasks}
+            className="btn btn-secondary max-w-xs mx-auto mt-5"
+          >
+            Load more tasks
+          </button>
+        )}
       </div>
-     
-      <AddTaskModal groupId={id} updateTasks={updateTasks}/>
+
+      <AddTaskModal groupId={id} updateTasks={updateTasks} />
     </div>
   );
 }
